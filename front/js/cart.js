7cdd467407a797a0baeae710638eba5abb55dcd5
RaZe <julian.drion@gmail.com>
2022-06-02 02:48:26 +0200

    Avancée 6

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,7 @@
 const elt1 = document.getElementById("cart__items");
 const produitTrouve = JSON.parse(localStorage.getItem("product"));
 
+// Fonction permettant de récupérer les données du LocalStorage pour remplacer le HTML par le produit sélectionné
 async function kanap1() {
   try {
     for (let kanap of produitTrouve) {
@@ -30,8 +31,126 @@ async function kanap1() {
       </div>
     </article>`;
     }
+    kanap2();
+    // Fonction permettant de changer la quantité depuis la page Panier
+    const inputs = document.querySelectorAll(".itemQuantity");
+    for (const input of inputs) {
+      input.addEventListener("change", function (e) {
+        const product = JSON.parse(localStorage.getItem("product"));
+        const majQuantity = e.target.value;
+        const produitSelectionne = {
+          id: e.target.closest("article").dataset.id,
+          couleur: e.target.closest("article").dataset.color,
+        };
+        const findProduct = product.find(
+          (elt) =>
+            elt.id == produitSelectionne.id &&
+            elt.couleur == produitSelectionne.couleur
+        );
+        if (findProduct) {
+          findProduct.quantite = Number(majQuantity);
+          localStorage.setItem("product", JSON.stringify(product));
+        }
+        kanap2();
+      });
+    }
+    // Fonction permettant de supprimer un article du panier
+    const elt3 = document.querySelectorAll(".deleteItem");
+    for (const supp of elt3) {
+      supp.addEventListener("click", function (e) {
+        const product = JSON.parse(localStorage.getItem("product"));
+        const produitSelectionne = {
+          id: e.target.closest("article").dataset.id,
+          couleur: e.target.closest("article").dataset.color,
+        };
+        const findProduct = product.find(
+          (elt) =>
+            elt.id == produitSelectionne.id &&
+            elt.couleur == produitSelectionne.couleur
+        );
+        if (findProduct) {
+          product.splice(product.indexOf(findProduct), 1);
+          localStorage.setItem("product", JSON.stringify(product));
+          e.target.closest("article").remove();
+        }
+        kanap2();
+      });
+    }
+    const submit = document.getElementById("order");
+    submit.addEventListener("click", function (e) {
+      e.preventDefault();
+      const firstName = document.getElementById("firstName").value;
+      validateFirstName();
+    });
   } catch {
     elt1.innerHTML = "Error Loading Elements";
   }
 }
 kanap1();
+
+async function kanap2() {
+  try {
+    const product = JSON.parse(localStorage.getItem("product"));
+    let price = 0;
+    let productTotal = 0;
+    for (let kanap of product) {
+      const response = await fetch(
+        `http://localhost:3000/api/products/${kanap.id}`
+      );
+      const data = await response.json();
+      price += data.price * kanap.quantite;
+      productTotal += kanap.quantite;
+    }
+    const totalArticle = document.getElementById("totalQuantity");
+    const totalPrice = document.getElementById("totalPrice");
+
+    totalArticle.innerHTML = productTotal;
+    totalPrice.innerHTML = price;
+  } catch {}
+}
+
+function validateFirstName(firstName) {
+  const firstNameReg = new RegExp(/[0-9]/);
+  const validFirstName = firstNameReg.test(firstName);
+  console.log(validFirstName);
+
+  if (validFirstName == false) {
+    alert("Prénom invalide");
+  }
+}
+
+function validateLastName(lastName) {
+  const lastNameReg = new RegExp(/^[A-zÀ-ú' -]+$/);
+  const validLastName = lastNameReg.test(lastName);
+
+  if (validLastName == false) {
+    alert("Nom invalide");
+  }
+}
+
+function validateAdress(adress) {
+  const adressReg = new RegExp(/([0-9]{1,}) ?([A-zÀ-ú,' -. ]*)/);
+  const validAdress = adressReg.test(adress);
+
+  if (validAdress == false) {
+    alert("Nom invalide");
+  }
+}
+
+function validateEmail(email) {
+  const emailReg = new RegExp(/^[A-z0-9-_.]{1,}[@][A-z-]{2,}[.][A-z]{2,}$/g);
+  const validEmail = emailReg.test(email);
+
+  if (validEmail == false) {
+    alert("Nom invalide");
+  }
+}
+// Détecter un événement positif (incrémentation de la quantité)
+
+// créer une fonction qui permet de modifier l'état du nombre
+// Créer une fonction de l'état de la quantité totale
+
+// Détecter un événement négatif (diminution de la quantité)
+
+// Créer une fonction qui permet de soustraire le nombre
+// Créer une fonction de l'état de la quantité totale
